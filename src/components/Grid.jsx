715e2300448df88b85b541d602706de9b5847405
Joseph Raub <joseph@artisanmade.io>
2023-09-16 10:28:10 -0500

    Render client side

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,33 +2,6 @@ import { Component } from "react"
 import _ from 'lodash'
 import Staff from "./Staff";
 
-// export default function Grid(props)
-// {
-//   return (
-//     <section className="text-gray-600 body-font">
-//       <div className="container px-2 py-8 mx-auto">
-
-//         <div className="flex flex-col text-center w-full mb-10">
-
-//           <h1 className="text-4xl font-bold title-font mb-4 text-gray-900">{props.title}</h1>
-
-//           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-
-//             {props.description}
-
-//           </p>
-//         </div>
-
-//         <div className="flex flex-wrap m-4 mx-auto lg:mx-0 justify-center lg:justify-start">
-            
-//             {props.children}
-        
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
 export default class extends Component
 {
   constructor(props) {
