082fb1d4047faf1a451ba09b7f4dc62fc2c4a8c4
Joseph Raub <josephraub98@gmail.com>
2025-12-23 11:20:10 -0600

    Empty project

--- src/components/RightContent.jsx
+++
@@ -1,8 +0,0 @@
-export default function RightContent(props)
-{
-    return (
-        <div className="flex justify-end bg-gray-300 my-6 px-6 w-full">
-            {props.children}
-        </div>
-    );
-}
--- src/styles.js
+++
@@ -1,6 +0,0 @@
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function classNames(...inputs) {
-	return twMerge(clsx(inputs));
-}
